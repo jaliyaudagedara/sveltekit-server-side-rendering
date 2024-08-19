4cb8e2bec3e2d8e6563cb7f0d58afa406015d3f1
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2024-08-19 18:31:28 +1200

    Fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="py-4">
-    <h1 class="font-semibold text-2xl pb-2">Demo SvelteKit erver-side rendering application!</h1>
+    <h1 class="font-semibold text-2xl pb-2">Demo SvelteKit Server-side rendering application!</h1>
 </div>
